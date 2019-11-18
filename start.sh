946db0fb958a844cdb346085ef05760c1c44f38e
thereapsz <alienware_me_rrk@live.no>
2019-11-18 19:30:43 +0100

    Fixed

--- start.sh
+++ start.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# -----------------------------------------------------------------------------
-# docker-minecraft /start script
-# -----------------------------------------------------------------------------
-
-#if [ ! -f /data/eula.txt ]
-#then
-#    echo "eula=true" > /data/eula.txt
-#fi
-
 echo eula=true > /data/eula.txt
 
-java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC \
-            -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 \
-            -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions \
+            -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC \
+            -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 \
+            -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 \
+            -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled \
+            -Dusing.aikars.flags=mcflags.emc.gs -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
